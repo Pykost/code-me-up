10b02d8eedfb8458a20c839b033d8185ec66f847
Pykost <47693527+Pykost@users.noreply.github.com>
2019-03-02 16:54:22 +0200

    initial commit

---
+++ pyfile3.py
@@ -0,0 +1,10 @@
+N=int(input('please give me a number '))
+for i in range(1,N+1):
+    if i%15==0:
+        print('FizzBuzz')
+    elif i%5==0:
+        print('Fizz')
+    elif i%3==0:
+        print('Buzz')
+    else:
+        print(i)
