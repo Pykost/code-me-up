503c7c1b9a5805b670fab0f0eac9ebb3fb7d102f
Pykost <47693527+Pykost@users.noreply.github.com>
2019-03-23 14:37:54 +0200

    Square perimeter and area

--- pyfile.py
+++ pyfile.py
@@ -1,6 +1,12 @@
+"""
+Finds the area and perimeter of a square 
+"""
 i = input ('Please enter a side ')
 i = int (i)
+
 j = input ('Please enter a different side ')
 j = int (j)
+
 print ('The perimeter of the square is', 2 * i + 2 * j )
+
 print ('The area of the square is', i * j )
