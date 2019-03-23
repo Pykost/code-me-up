70d28cc3ca455b42cfb7b25c66349d28f4bc5334
Pykost <47693527+Pykost@users.noreply.github.com>
2019-03-23 14:51:42 +0200

    added a comment

--- pyfile4.py
+++ pyfile4.py
@@ -1,8 +1,13 @@
 N=int(input('please give me a number '))
+
 while N < 0:
     N = input('please give me a number ')
     N = int(N)
+    
+#While added so it loops every time a negative number is given until the user gives the expected answer.
+
 for i in range(1,N+1):
     if i%3==0:
         x=i
+        
 print (x)
